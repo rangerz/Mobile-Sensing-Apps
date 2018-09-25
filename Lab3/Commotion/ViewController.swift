22c0cbbfaa25cded3ebf2a6771c168ce7f12e205
Ranger Chan <jchiehc@gmail.com>
2018-09-25 04:43:13 -0500

    init Lab3 from BareMotionUpdates and WithBounceGame branch

---
+++ Lab3/Commotion/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Commotion
+//
+//  Created by Eric Larson on 9/6/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+    
+    //MARK: class variables
+    let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
+    let motion = CMMotionManager()
+    var totalSteps: Float = 0.0 {
+        willSet(newtotalSteps){
+            DispatchQueue.main.async{
+                self.stepsSlider.setValue(newtotalSteps, animated: true)
+                self.stepsLabel.text = "Steps: \(newtotalSteps)"
+            }
+        }
+    }
+    
+    //MARK: UI Elements
+    @IBOutlet weak var stepsSlider: UISlider!
+    @IBOutlet weak var stepsLabel: UILabel!
+    @IBOutlet weak var isWalking: UILabel!
+    
+    
+    //MARK: View Hierarchy
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.totalSteps = 0.0
+        self.startActivityMonitoring()
+        self.startPedometerMonitoring()
+        self.startMotionUpdates()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Raw Motion Functions
+    func startMotionUpdates(){
+        // some internal inconsistency here: we need to ask the device manager for device 
+        
+        // TODO: should we be doing this from the MAIN queue? You will need to fix that!!!....
+        if self.motion.isDeviceMotionAvailable{
+            self.motion.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: handleMotion)
+        }
+    }
+    
+    func handleMotion(_ motionData:CMDeviceMotion?, error:Error?){
+        if let gravity = motionData?.gravity {
+            let rotation = atan2(gravity.x, gravity.y) - Double.pi
+            self.isWalking.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
+        }
+    }
+    
+    // MARK: Activity Functions
+    func startActivityMonitoring(){
+        // is activity is available
+        if CMMotionActivityManager.isActivityAvailable(){
+            // update from this queue (should we use the MAIN queue here??.... )
+            self.activityManager.startActivityUpdates(to: OperationQueue.main, withHandler: self.handleActivity)
+        }
+        
+    }
+    
+    func handleActivity(_ activity:CMMotionActivity?)->Void{
+        // unwrap the activity and disp
+        if let unwrappedActivity = activity {
+            DispatchQueue.main.async{
+                self.isWalking.text = "Walking: \(unwrappedActivity.walking)\n Still: \(unwrappedActivity.stationary)"
+            }
+        }
+    }
+    
+    // MARK: Pedometer Functions
+    func startPedometerMonitoring(){
+        //separate out the handler for better readability
+        if CMPedometer.isStepCountingAvailable(){
+            pedometer.startUpdates(from: Date(),withHandler: self.handlePedometer )
+        }
+    }
+    
+    //ped handler
+    func handlePedometer(_ pedData:CMPedometerData?, error:Error?){
+        if let steps = pedData?.numberOfSteps {
+            self.totalSteps = steps.floatValue
+        }
+    }
+
+
+}
+
